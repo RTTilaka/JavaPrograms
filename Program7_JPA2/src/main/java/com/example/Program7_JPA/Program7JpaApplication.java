9585ee55e07b0a8fc51494da8a95d8eda6857085
RTTilaka <154359692+RTTilaka@users.noreply.github.com>
2024-02-28 07:47:16 +0530

    Add files via upload

---
+++ Program7_JPA2/src/main/java/com/example/Program7_JPA/Program7JpaApplication.java
@@ -0,0 +1,11 @@
+package com.example.Program7_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program7JpaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Program7JpaApplication.class, args);
+	}
+}
