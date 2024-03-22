b7823ef0f09b726f6b20a3664709a84ccd2cda69
RTTilaka <154359692+RTTilaka@users.noreply.github.com>
2024-03-23 02:48:52 +0530

    Program10

---
+++ Program10/src/main/java/com/example/Program10/Program10Application.java
@@ -0,0 +1,13 @@
+package com.example.Program10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program10Application.class, args);
+	}
+
+}
